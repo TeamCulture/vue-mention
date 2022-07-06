09460de5a26fe70de14a644b7ec0e92ab3c421aa
Jhonatan Kennedy <jhonatankennedy@live.com>
2022-07-06 19:07:05 -0300

    fix space

---
+++ packages/test-e2e/src/views/AllowSpace.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="demo">
+    <Mentionable
+      :keys="['@']"
+      :items="items"
+      allow-space
+      popper-class="defaults-mention"
+    >
+      <textarea v-model="text" rows="6" class="input" />
+    </Mentionable>
+
+    <div class="preview">{{ text }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      text: '',
+      items: [
+        {
+          value: 'akryum',
+          firstName: 'Guillaume',
+        },
+        {
+          value: 'posva',
+          firstName: 'Eduardo',
+        },
+        {
+          value: 'space invader',
+          firstName: 'Space Invader',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style>
+.defaults-mention .mention-selected {
+  background: #aaffc7;
+}
+</style>
