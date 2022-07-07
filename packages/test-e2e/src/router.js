114173313fd57b76f66e556792aef8d9ce67b2dc
Jhonatan Kennedy <jhonatankennedy@live.com>
2022-07-06 22:22:41 -0300

    adiçao da possibilidade de manter o espaço na mention
    lint fix

--- packages/test-e2e/src/router.js
+++ packages/test-e2e/src/router.js
@@ -1,70 +1,75 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from './views/Home.vue'
-import Defaults from './views/Defaults.vue'
-import MultipleCollections from './views/MultipleCollections.vue'
-import Events from './views/Events.vue'
-import InsertSpace from './views/InsertSpace.vue'
-import OmitKey from './views/OmitKey.vue'
-import MapInsert from './views/MapInsert.vue'
-import Limit from './views/Limit.vue'
-import ItemSlot from './views/ItemSlot.vue'
-import WrappedInput from './views/WrappedInput.vue'
-import ContentEditable from './views/ContentEditable.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./views/Home.vue";
+import Defaults from "./views/Defaults.vue";
+import MultipleCollections from "./views/MultipleCollections.vue";
+import Events from "./views/Events.vue";
+import InsertSpace from "./views/InsertSpace.vue";
+import OmitKey from "./views/OmitKey.vue";
+import MapInsert from "./views/MapInsert.vue";
+import Limit from "./views/Limit.vue";
+import ItemSlot from "./views/ItemSlot.vue";
+import WrappedInput from "./views/WrappedInput.vue";
+import ContentEditable from "./views/ContentEditable.vue";
+import AllowSpace from "./views/AllowSpace.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: Home,
   },
   {
-    path: '/defaults',
+    path: "/defaults",
     component: Defaults,
   },
   {
-    path: '/multiple-collections',
+    path: "/multiple-collections",
     component: MultipleCollections,
   },
   {
-    path: '/search-event',
+    path: "/search-event",
     component: Events,
   },
   {
-    path: '/insert-space',
+    path: "/insert-space",
     component: InsertSpace,
   },
   {
-    path: '/omit-key',
+    path: "/omit-key",
     component: OmitKey,
   },
   {
-    path: '/map-insert',
+    path: "/map-insert",
     component: MapInsert,
   },
   {
-    path: '/limit',
+    path: "/limit",
     component: Limit,
   },
   {
-    path: '/item-slot',
+    path: "/item-slot",
     component: ItemSlot,
   },
   {
-    path: '/wrapped-input',
+    path: "/wrapped-input",
     component: WrappedInput,
   },
   {
-    path: '/content-editable',
+    path: "/content-editable",
     component: ContentEditable,
   },
-]
+  {
+    path: "/allow-space",
+    component: AllowSpace,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
-})
+});
 
-export default router
+export default router;
